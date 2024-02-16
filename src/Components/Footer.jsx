7715e6cf7c11041e2998aedade45c69fc78e8060
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-02-16 22:46:46 +0100

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
             TechFam aims to change the skills landscape in Africa through various initiatives with objective to help African
             women acquire digital skills for today's workspaces and tomorrow's needs. We hope to influence the now and set the tone for the future.
           </p>
-          <div className='techfam-sm'>
+          <div className='techfam-sm desktop'>
             <a href="" target="_blank" rel="noopener noreferrer">
               <img src={Facebook} />
             </a>
@@ -39,63 +39,80 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div className='links'>
-          <h4>
-            Quick Links
-          </h4>
-          <ul>
-            <Link to="/blog">
-              <li>
-                Blog
-              </li>
-            </Link>
-            <Link to="/apply">
-              <li>
-                Apply
-              </li>
-            </Link>
-            <Link to="/termsOfService">
-              <li>
-                Terms of Service
-              </li>
-            </Link>
-            <Link to="/privacyPolicy">
-              <li>
-                Privacy Policy
-              </li>
-            </Link>
-            <Link to="/donate">
-              <li>
-                Donate
-              </li>
-            </Link>
-          </ul>
+        <div className='link-grid'>
+          <div className='links'>
+            <h4>
+              Quick Links
+            </h4>
+            <ul>
+              <Link to="/blog">
+                <li>
+                  Blog
+                </li>
+              </Link>
+              <Link to="/apply">
+                <li>
+                  Apply
+                </li>
+              </Link>
+              <Link to="/termsOfService">
+                <li>
+                  Terms of Service
+                </li>
+              </Link>
+              <Link to="/privacyPolicy">
+                <li>
+                  Privacy Policy
+                </li>
+              </Link>
+              <Link to="/donate">
+                <li>
+                  Donate
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <div className='contact'>
+            <h4>
+              Contact
+            </h4>
+            <div className='contact-cont'>
+              <a href="tel:+2347030940388" target="_blank" rel="noopener noreferrer">
+                <img src={Phone} />
+                <p>+234 703 094 0388</p>
+              </a>
+            </div>
+            <div className='contact-cont'>
+              <a href="mailto:hello@techfam.online" target="_blank" rel="noopener noreferrer">
+                <img src={Email} />
+                <p>
+                  hello@techfam.online
+                </p>
+              </a>
+            </div>
+            <div className='contact-cont'>
+              <a href="https://www.google.com/maps?q=Lekki, Lagos State, Nigeria" target="_blank" rel="noopener noreferrer">
+                <img src={Location} />
+                <p>Lekki, Lagos State, Nigeria</p>
+              </a>
+            </div>
+            <div className='techfam-sm mobile'>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <img src={Facebook} />
+              </a>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <img src={Instagram} />
+              </a>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <img src={Twitter} />
+              </a>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <img src={Youtube} />
+              </a>
+          </div>
+          </div>
         </div>
-        <div className='contact'>
-          <h4>
-            Contact
-          </h4>
-          <div className='contact-cont'>
-            <a href="tel:+2347030940388" target="_blank" rel="noopener noreferrer">
-              <img src={Phone} />
-              <p>+234 703 094 0388</p>
-            </a>
-          </div>
-          <div className='contact-cont'>
-            <a href="mailto:hello@techfam.online" target="_blank" rel="noopener noreferrer">
-              <img src={Email} />
-              <p>
-                hello@techfam.online
-              </p>
-            </a>
-          </div>
-          <div className='contact-cont'>
-            <a href="https://www.google.com/maps?q=Lekki, Lagos State, Nigeria" target="_blank" rel="noopener noreferrer">
-              <img src={Location} />
-              <p>Lekki, Lagos State, Nigeria</p>
-            </a>
-          </div>
-        </div>
+        
         <div className='newsletter'>
           <h4>Newsletter</h4>
           <p> Stay tuned for upcoming events and opportunities to get involved with our mission</p>
