c1a429c03bb7486e7924683744edac5e522e207e
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-04-25 11:02:24 +0100

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Logo1 from '../assets/logo1.png';
-import { Link, useLocation } from 'react-router-dom';
-import Menubar from '../assets/menu.svg';
-import '../Components/CSS/Header.css';
+import React, { useEffect, useRef, useState } from "react";
+import Logo1 from "../assets/logo1.png";
+import { Link, useLocation } from "react-router-dom";
+import Menubar from "../assets/menu.svg";
+import "../Components/CSS/Header.css";
 
-const Header = ({scroll, showMobileNav}) => {
+const Header = ({ scroll, showMobileNav }) => {
   const location = useLocation();
   const elementRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
   useEffect(() => {
     const options = {
       root: null,
-      rootMargin: '0px',
+      rootMargin: "0px",
       threshold: 0.5,
     };
 
     const callback = (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          console.log('Element is in view');
+          console.log("Element is in view");
           setIsVisible(true);
           observer.unobserve(entry.target);
         }
       });
-    }
+    };
 
     const observer = new IntersectionObserver(callback, options);
 
@@ -33,68 +33,75 @@ const Header = ({scroll, showMobileNav}) => {
 
     return () => observer.disconnect();
   }, []);
-  
+
   return (
-    
-    <div className={`header ${isVisible ? 'visible' : ''}`} ref={elementRef}>
-      <div className='logo'>
-        <img src={Menubar} className='menu' onClick={() => {showMobileNav()}} />
+    <div className={`header ${isVisible ? "visible" : ""}`} ref={elementRef}>
+      <div className="logo">
+        <img
+          src={Menubar}
+          className="menu"
+          onClick={() => {
+            showMobileNav();
+          }}
+        />
         <Link to="/">
           <img src={Logo1} alt="website-logo" />
         </Link>
       </div>
-      <div className='header-links'>
+      <div className="header-links">
         <ul>
           <Link to="/">
-            <li>
-              Home
-            </li>
-            <div className={location.pathname === '/' ? 'link-border' : ''}></div>
-          </Link> 
-          <div className='are-link'>
-            <li onClick={() => {scroll()}}>
+            <li>Home</li>
+            <div
+              className={location.pathname === "/" ? "link-border" : ""}
+            ></div>
+          </Link>
+          <div className="are-link">
+            <li
+              onClick={() => {
+                scroll();
+              }}
+            >
               Area Of Focus
-            </li>  
-            <div className={location.pathname === '/areaoffocus' ? 'link-border' : ''}></div>
+            </li>
+            <div
+              className={
+                location.pathname === "/areaoffocus" ? "link-border" : ""
+              }
+            ></div>
           </div>
           <Link to="/events">
-            <li>
-              Events
-            </li>
-            <div className={location.pathname === '/events' ? 'link-border' : ''}></div>
-          </Link> 
+            <li>Events</li>
+            <div
+              className={location.pathname === "/events" ? "link-border" : ""}
+            ></div>
+          </Link>
           <Link to="/about">
-            <li>
-              About
-            </li>
-            <div className={location.pathname === '/about' ? 'link-border' : ''}></div>
-          </Link> 
+            <li>About</li>
+            <div
+              className={location.pathname === "/about" ? "link-border" : ""}
+            ></div>
+          </Link>
           <Link to="/contact">
-            <li>
-              Contact
-            </li>  
-            <div className={location.pathname === '/contact' ? 'link-border' : ''}></div>
-          </Link> 
+            <li>Contact</li>
+            <div
+              className={location.pathname === "/contact" ? "link-border" : ""}
+            ></div>
+          </Link>
         </ul>
       </div>
-      <div className='header-cont3'>
-        <div className='donate-btn'>
-          <button>
-            <Link to="/donate">
-              Donate
-            </Link>
-          </button>
-        </div>
+      <div className="header-cont3">
+        <Link to="/donate" className="donate-btn">
+          <button>Donate</button>
+        </Link>
       </div>
-      <div className='donate-btn mobil'>
+      <div className="donate-btn mobil">
         <button>
-          <Link to="/donate">
-            Donate
-          </Link>
+          <Link to="/donate">Donate</Link>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
